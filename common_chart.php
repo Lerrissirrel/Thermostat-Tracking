608ec3bf8a62ee0d2449d973e7185189a4528e73
Lerrissirrel <Lerrissirrel@users.noreply.github.com>
2025-08-17 20:45:11 -0400

    Change charting package to echarts

--- common_chart.php
+++ common_chart.php
@@ -1,30 +1,16 @@
 <?php
 require_once( 'common.php' );
 
-// Modify the font path for the GD library - because graphic renders are lame?
-// Must use absolute and not relative paths.
-$pChart_fontpath = realpath( '../../common/php/pChart/fonts' );														// font path for this application
-$my_fontpath = realpath( 'lib/fonts' );                                                                                                         // font path for this application
-putenv( 'GDFONTPATH='.$pChart_fontpath.PATH_SEPARATOR.$my_fontpath );
+session_write_close();
 
+// Modify the font path for the GD library.  Must use absolute and not relative paths.
+// Should probably go in config.php
+$my_fontpath = realpath( 'lib/fonts' );
+putenv( 'GDFONTPATH='.$my_fontpath );
 
-require_once ('c-pChart/constants.php');
-require_once ('c-pChart/src/BaseDraw.php');
-require_once ('c-pChart/src/Draw.php');
-require_once ('c-pChart/src/Cache.php');
-require_once ('c-pChart/src/Image.php');
-require_once ('c-pChart/src/Data.php');
-require_once ('c-pChart/src/Chart/Bubble.php');
-require_once ('c-pChart/src/Chart/Indicator.php');
-require_once ('c-pChart/src/Chart/Pie.php');
-require_once ('c-pChart/src/Chart/Radar.php');
-require_once ('c-pChart/src/Chart/Scatter.php');
-require_once ('c-pChart/src/Chart/Split.php');
-require_once ('c-pChart/src/Chart/Spring.php');
-require_once ('c-pChart/src/Chart/Stock.php');
-require_once ('c-pChart/src/Chart/Surface.php');
-
-
+// Poor Bobby Tables is broken at the moment since the method of drawing the charts changed to
+// place them explicitly in their divs, so Bobby needs some rework.  He's a nice boy, though, so
+// we should keep him.
 // Replaces chart with anti-hacking graphic (usually when web user has used a mal-formed date string)
 function bobby_tables()
 {
@@ -40,7 +26,8 @@ function validate_date( $some_date )
 	$date_pattern = "/[2]{1}[0]{1}[0-9]{2}-[0-9]{2}-[0-9]{2}/";
 	if( !preg_match( $date_pattern, $some_date ) || strlen($some_date) != 10)
 	{	// I want it to be EXACTLY YYYY-MM-DD
-		bobby_tables();
+                $log->Error("bogus date: ".$some_date);
+//		bobby_tables();
 		return false;
 	}
 	return true;
@@ -49,18 +36,24 @@ function validate_date( $some_date )
 // Common code that should run for EVERY CHART page follows here
 $id = (isset($_REQUEST['id'])) ? $_REQUEST['id'] : null;    // Set id to chosen thermost (or null if not chosen)
 if( $id == null )
-{ // If the thermostat to display was not chosen, choose one
-	$thermostat = array_pop($thermostats);
-	if( is_array($thermostat) && isset($thermostat['id']))
-	{
-		$id = $thermostat['id'];
-	}
+{ 
+   // This is dubious.  1) When would it happen and 2) Do we really want to see a random stat's data if it does?
+   // If the thermostat to display was not chosen, choose one
+   $thermostat = array_pop($thermostats);
+   if( is_array($thermostat) && isset($thermostat['id']))
+   {
+      $id = $thermostat['id'];
+      $log->Error("common_chart.php: We picked a random stat (".$id.") because one wasn't set.  Really.");
+   }
 }
+
+// We STILL have a null stat id??
 if( $id == null )
-{ // If there still is not one chosen then abort
-	$log->logError( 'common_chart.php: Thermostat ID was NULL!' );
-	// Need to redirect output to some image showing user there was an error and suggesting to read the logs.
-	return;
+{ 
+   // If there still is not one chosen then abort
+   $log->Error( 'common_chart.php: Thermostat ID was NULL!' );
+   // Need to redirect output to some image showing user there was an error and suggesting to read the logs.
+   return;
 }
 
 // Having now chosen a thermostat to display, gather information about it.
@@ -69,12 +62,13 @@ $query = $pdo->prepare( $sql );
 $query->execute( array( $id ) );
 $thermData = $query->fetchAll();
 if( !isset($thermData[0]['tstat_uuid']) || empty($thermData[0]['tstat_uuid']))
-{ // If the chosen thermostat is not known to the system then abort
-	$log->logError( 'common_chart.php: Requested thermostat ID was not found!' );
-	// Need to redirect output to some image showing user there was an error and suggesting to read the logs.
-	return;
+{ 
+   // If the chosen thermostat is not known to the system then abort
+   $log->Error( 'common_chart.php: Requested thermostat ID was not found!' );
+   // Need to redirect output to some image showing user there was an error and suggesting to read the logs.
+   return;
 }
 $uuid = $thermData[0]['tstat_uuid'];
 $statName = $thermData[0]['name'];
-
+$log->Info("Common_chart.php: id: ".$id." uuid: ".$uuid." named: ".$statName);
 ?>
